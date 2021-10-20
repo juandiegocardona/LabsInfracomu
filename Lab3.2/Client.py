dabc7b11262c31955521fc7d826e1cf630f2063f
Gabriela Caballero <g.caballero@uniandes.edu.co>
2021-10-19 19:52:48 -0500

    avances

--- Lab3.2/Client.py
+++ Lab3.2/Client.py
@@ -1,31 +1,38 @@
 import socket
+import select
 import time
 from datetime import datetime
 
-TCP_IP = 'localhost'
-TCP_PORT = 9001
-BUFFER_SIZE = 1024
+UDP_IP = socket.gethostname()
+IN_PORT = 9999
+timeout = 3
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+fecha = datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((UDP_IP, IN_PORT))
+
+
 log = datetime.now().strftime("%d-%m-%Y | %H:%M:%S") + " | "
-recived_f = 'imgt_thread'+str(time.time()).split('.')[0]+'.txt'
-recived_f += log
-with open(recived_f, 'wb') as f:
-    print('file opened')
+log_file = open('C'  + "-" + datetime.now().strftime("%d-%m-%Y_%H_%M_%S.txt"), 'w')
+log_file.write(log)
+log_file.close()
+
+
+while True:
+    data, addr = s.recvfrom(1024)
+    if data:
+        print("File name:", data)
+        file_name = data.strip()
+
+    f = open(file_name, 'wb')
+
     while True:
-        #print('receiving data...')
-        data = s.recv(BUFFER_SIZE)
-        print('data=%s', (data))
-        if not data:
+        ready = select.select([s], [], [], timeout)
+        if ready[0]:
+            data, addr = s.recvfrom(1024)
+            f.write(data)
+        else:
+            print ("%s Finish!" % file_name)
             f.close()
-            print('file close()')
             break
-        # write data to a file
-        f.write(data)
-
-
-
-print('Successfully get the file')
-s.close()
-print('connection closed')
+ 
--- Lab3.2/Server.py
+++ Lab3.2/Server.py
@@ -24,83 +24,20 @@ num_clientes = int(input("Ingrese numero de clientes: "))
 print("Esperando {} clientes".format(num_clientes))
 
 
-TCP_IP = 'localhost'
-TCP_PORT = 9001
-BUFFER_SIZE = 1024
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+host =socket.gethostname()
+port = 9999
+buf =1024
+addr = (host,port)
 
 
-class ClientThread(Thread):
+s.sendto(str.encode(file_path),addr)
 
-    def __init__(self, ip, port, sock):
-        Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.sock = sock
-        print(" New thread started for "+ip+":"+str(port))
-
-    def run(self):
-        filename = file_path
-        f = open(filename, 'rb')
-        while True:
-            l = f.read(BUFFER_SIZE)
-            while (l):
-                self.sock.send(l)
-                #print('Sent ',repr(l))
-                l = f.read(BUFFER_SIZE)
-            if not l:
-                f.close()
-                self.sock.close()
-                break
-
-
-tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind((TCP_IP, TCP_PORT))
-threads = []
-
-def threaded(c, thread_count):
-    client_ready = c.recv(BUFFER_SIZE).decode()
-    if client_ready == "Preparado":
-        
-        with c,open(file_path, 'rb') as f:
-            
-            filesize = f'{os.path.getsize(file_path)}'
-
-            c.sendall(str(thread_count).encode()+b'\n')
-            c.sendall(file_name.encode()+b'\n')
-            c.sendall(filesize.encode() + b'\n')
-
-            log = "C" + str(thread_count) + " | " + fecha + " | "
-            log = log + file_name + " | " + str(round((int(filesize)/1024),2)) + "KB"
-            inicio = time.time()
-            while True:
-                data = f.read(BUFFER_SIZE)
-                if not data: break
-                c.sendall(data)
-            client_recibido = c.recv(BUFFER_SIZE).decode()
-            if client_recibido == "Recibido":
-                print("Enviado con exito a cliente {}".format(thread_count))
-                c.sendall(str(inicio).encode()+b'\n')
-                tiempo = float(c.recv(BUFFER_SIZE).decode())
-                log = log + " | Tiempo: " + str(tiempo) + "s | Paquetes: "+str(int(filesize)/BUFFER_SIZE)+" | Exitoso "+"\n"
-            else:
-                print("Errores en el envio a cliente {}".format(thread_count))
-                log = log + " | Error\n"
-    c.close()
-    log_file = open("./logs/"+fecha+".txt",'a')
-    log_file.write(log)
-    log_file.close()
-
-
-while True:
-    tcpsock.listen(5)
-    print("Waiting for incoming connections...")
-    (conn, (ip, port)) = tcpsock.accept()
-    print('Got connection from ', (ip, port))
-    newthread = ClientThread(ip, port, conn)
-    newthread.start()
-    threads.append(newthread)
-
-    for t in threads:
-       t.join()
-
+f=open(file_path,"rb")
+data = f.read(buf)
+while (data):
+    if(s.sendto(data,addr)):
+        print("sending ...")
+        data = f.read(buf)
+s.close()
+f.close()
